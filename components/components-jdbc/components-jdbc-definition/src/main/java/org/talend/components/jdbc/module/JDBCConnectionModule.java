8deb5fee2996222fd5ecc7175982be3328f81aeb
wang wei <wwang@talend.com>
2017-10-26 13:57:45 +0800

    fix(TUP-18543): provide the ui to allow to user to select the class from the jdbc driver in the wizard part

--- components/components-jdbc/components-jdbc-definition/src/main/java/org/talend/components/jdbc/module/JDBCConnectionModule.java
+++ components/components-jdbc/components-jdbc-definition/src/main/java/org/talend/components/jdbc/module/JDBCConnectionModule.java
@@ -14,9 +14,20 @@ package org.talend.components.jdbc.module;
 
 import static org.talend.daikon.properties.presentation.Widget.widget;
 
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.sql.Driver;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+
 import org.talend.components.api.properties.ComponentPropertiesImpl;
 import org.talend.components.common.UserPasswordProperties;
 import org.talend.components.jdbc.CommonUtils;
+import org.talend.components.jdbc.runtime.setting.AllSetting;
+import org.talend.daikon.properties.PresentationItem;
+import org.talend.daikon.properties.ValidationResult;
 import org.talend.daikon.properties.presentation.Form;
 import org.talend.daikon.properties.presentation.Widget;
 import org.talend.daikon.properties.property.Property;
@@ -35,6 +46,8 @@ public class JDBCConnectionModule extends ComponentPropertiesImpl {
     public DriverTable driverTable = new DriverTable("driverTable");
 
     public Property<String> driverClass = PropertyFactory.newProperty("driverClass").setRequired();
+
+    public PresentationItem selectClass = new PresentationItem("selectClass");
 
     public UserPasswordProperties userPassword = new UserPasswordProperties("userPassword");
 
@@ -56,7 +69,80 @@ public class JDBCConnectionModule extends ComponentPropertiesImpl {
         form.addRow(jdbcUrl);
         form.addRow(widget(driverTable).setWidgetType(Widget.TABLE_WIDGET_TYPE));
         form.addRow(driverClass);
+        //hide the button firstly as the ui is not ok now, but the trigger method can work now already
+        form.addColumn(widget(selectClass).setWidgetType(Widget.BUTTON_WIDGET_TYPE).setHidden(true));
         form.addRow(userPassword.getForm(Form.MAIN));
+    }
+
+    public ValidationResult afterSelectClass() {
+        List<String> driverClasses = new ArrayList<>();
+
+        AllSetting setting = new AllSetting();
+        setting.setDriverPaths(driverTable.drivers.getValue());
+        List<String> jar_maven_paths = setting.getDriverPaths();
+
+        try {
+            List<URL> urls = new ArrayList<>();
+            for (String maven_path : jar_maven_paths) {
+                URL url = new URL(removeQuote(maven_path));
+                urls.add(url);
+            }
+
+            URLClassLoader classLoader = new URLClassLoader(urls.toArray(new URL[0]), this.getClass().getClassLoader());
+
+            for (URL jarUrl : urls) {
+                try (JarInputStream jarInputStream = new JarInputStream(jarUrl.openStream())) {
+                    JarEntry nextJarEntry = jarInputStream.getNextJarEntry();
+                    while (nextJarEntry != null) {
+                        boolean isFile = !nextJarEntry.isDirectory();
+                        if (isFile) {
+                            String name = nextJarEntry.getName();
+                            if (name != null && name.toLowerCase().endsWith(".class")) {
+                                String className = changeFileNameToClassName(name);
+                                try {
+                                    Class clazz = classLoader.loadClass(className);
+                                    if (Driver.class.isAssignableFrom(clazz)) {
+                                        driverClasses.add(clazz.getName());
+                                    }
+                                } catch (Throwable th) {
+                                    // ignore all the exceptions, especially the class not found exception when look up a class
+                                    // outside the jar
+                                }
+                            }
+                        }
+
+                        nextJarEntry = jarInputStream.getNextJarEntry();
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            return new ValidationResult(ValidationResult.Result.ERROR, ex.getMessage());
+        }
+
+        if (driverClasses.isEmpty()) {
+            return new ValidationResult(ValidationResult.Result.ERROR,
+                    "not found any Driver class, please make sure the jar is right");
+        }
+
+        driverClass.setPossibleValues(driverClasses);
+        driverClass.setValue(driverClasses.get(0));
+
+        return ValidationResult.OK;
+    }
+
+    private String changeFileNameToClassName(String name) {
+        name = name.replace('/', '.');
+        name = name.replace('\\', '.');
+        name = name.substring(0, name.length() - 6);
+        return name;
+    }
+
+    private String removeQuote(String content) {
+        if (content.startsWith("\"") && content.endsWith("\"")) {
+            return content.substring(1, content.length() - 1);
+        }
+
+        return content;
     }
 
     public void setNotRequired() {
