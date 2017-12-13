31a44b033df6da8467c4e3a7c6237ce4b639c6f5
undx <emmanuel.gallois@gmail.com>
2017-12-13 18:16:43 +0000

    chore(TDI-39298): put Datastore ServiceAccount file in System properties
    - add IT for datastore/dataset
    - add a readme file for datastore settings

--- components/components-google-drive/components-google-drive-definition/src/main/java/org/talend/components/google/drive/data/GoogleDriveDatastoreProperties.java
+++ components/components-google-drive/components-google-drive-definition/src/main/java/org/talend/components/google/drive/data/GoogleDriveDatastoreProperties.java
@@ -2,7 +2,16 @@ package org.talend.components.google.drive.data;
 
 import static org.talend.daikon.properties.property.PropertyFactory.newProperty;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.talend.components.common.datastore.DatastoreProperties;
+import org.talend.daikon.i18n.GlobalI18N;
+import org.talend.daikon.i18n.I18nMessages;
 import org.talend.daikon.properties.PropertiesImpl;
 import org.talend.daikon.properties.presentation.Form;
 import org.talend.daikon.properties.property.Property;
@@ -11,13 +20,41 @@ public class GoogleDriveDatastoreProperties extends PropertiesImpl implements Da
 
     public Property<String> serviceAccountJSONFile = newProperty("serviceAccountJSONFile").setRequired();
 
+    public static final String SERVICE_ACCOUNT_FILE = "org.talend.components.google.drive.service_account_file";
+
+    private static final I18nMessages messages = GlobalI18N.getI18nMessageProvider()
+            .getI18nMessages(GoogleDriveDatastoreProperties.class);
+
+    private transient static final Logger LOG = LoggerFactory.getLogger(GoogleDriveDatastoreProperties.class);
+
     public GoogleDriveDatastoreProperties(String name) {
         super(name);
     }
 
     @Override
+    public void setupProperties() {
+        super.setupProperties();
+
+        String saPath = System.getProperty(SERVICE_ACCOUNT_FILE);
+        if (StringUtils.isEmpty(saPath)) {
+            LOG.warn((messages.getMessage("error.env.not.set")));
+            saPath = this.getClass().getClassLoader().getResource("service_account.json").getFile();
+        }
+        try {
+            FileInputStream is = new FileInputStream(saPath);
+            try {
+                is.close();
+            } catch (IOException e) {
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(messages.getMessage("error.file.not.found"));
+        }
+        serviceAccountJSONFile.setValue(saPath);
+    }
+
+    @Override
     public void setupLayout() {
         Form mainForm = Form.create(this, Form.MAIN);
-        mainForm.addRow(serviceAccountJSONFile);
+        // mainForm.addRow(serviceAccountJSONFile);
     }
 }
